cdd4fb1ae61253d90b39d83d5ca31da6fec01be4
alkihuri <alkihuri@gmail.com>
2021-01-08 09:52:00 +0200

    Second commit
    Added a simple scene with a controller for a primitive character

---
+++ VikingWalker/Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical") / 3;
+        float h = Input.GetAxis("Horizontal") / 3;
+        transform.position = transform.position + new Vector3(h, 0, v);
+    }
+}
