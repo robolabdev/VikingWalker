b27eb3ae02ad83b5f73542e09fe8a9511a391018
alkihuri <alkihuri@gmail.com>
2021-01-08 12:00:42 +0200

    Third Commit
    Intentional errors

--- VikingWalker/Assets/Scripts/Movement.cs
+++ VikingWalker/Assets/Scripts/Movement.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class Move ment : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Start 
     {
         
     }
