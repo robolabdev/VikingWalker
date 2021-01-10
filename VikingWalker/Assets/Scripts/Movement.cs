02057f2ed7cac8e043f63acda33be1834cfab38e
ActuallWerd <77228730+ActuallWerd@users.noreply.github.com>
2021-01-10 12:53:56 +0200

    Error Fix (movement.cs)
    Я короче ошибки за вас поправил ленивцы вы мои. Не благодорите.

---
+++ VikingWalker/Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    [SerializeField] CharacterController playerController;
+    // Start is called before the first frame update
+    void start() 
+    {
+        playerController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical") / 3;
+        float h = Input.GetAxis("Horizontal") / 3;
+        Vector3 direction = transform.forward * v + transform.right * h; 
+        transform.position = transform.position + new Vector3(h, 0, v);
+        Debug.Log("CurrentSpeed = " + playerController.velocity.magnitude);
+
+    }
+}
