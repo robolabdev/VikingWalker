98b6f8b54311362e442bbf1562bce4cab63648fa
twilightowl0 <sofalba@yandex.ru>
2021-01-10 12:25:28 +0300

    movement changes
    Checkpoint3(apprentice)

---
+++ VikingWalker/Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] CharacterController playerController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GetComponent<CharacterController>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical") / 3;
+        float h = Input.GetAxis("Horizontal") / 3;
+       Vector3 direction = transform.forward * v + transform.right * v;
+        playerController.SimpleMove(direction);
+        Debug.Log("Current speed = " + playerController.velocity.magnitude);
+    }
+}
