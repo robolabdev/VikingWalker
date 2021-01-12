3df458f1ed75dc055e453d96f48f35a67a88baa5
alkihuri <alkihuri@gmail.com>
2021-01-12 21:56:43 +0200

    Second lesson

--- VikingWalker/Assets/Scripts/Movement.cs
+++ VikingWalker/Assets/Scripts/Movement.cs
@@ -2,22 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move ment : MonoBehaviour
+public class Movement : MonoBehaviour
 {
-    
 
+    [SerializeField] CharacterController cc;
     // Start is called before the first frame update
-    void start 
+    void Start ()
     {
-         
+        cc = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float v = Input.GetAxis("Vertical") / 3;
-        float h = Input.GetAxis("Horizontal") / 3;
-        transform.position = transform.position + new Vector3(h, 0, v); 
-
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+        Vector3 direction = transform.forward * v + transform.right * h; 
+        cc.SimpleMove(direction);
     }
 }
