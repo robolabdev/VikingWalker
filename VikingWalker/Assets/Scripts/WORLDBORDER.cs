a86a68ba66544948928bc7aa495620dcc6f8777c
Matrixko <matvey.masko@gmail.com>
2021-01-31 11:24:29 +0200

    location update
    location- mountains volcano
    game realization- drowning, death in lava, cinemachine cam and world boarders

---
+++ VikingWalker/Assets/Scripts/WORLDBORDER.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WORLDBORDER : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        SceneManager.LoadScene("mathew");
+    }
+    // Update is called once per frame
+  
+}
