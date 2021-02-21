134a2630fb66eb5639e46905731d85281dfb2ac6
SaveliyFrolikov <thecoder13oops@gmail.com>
2021-02-21 11:28:40 +0200

    New house crafting
    Now you can craft a house to live in!

---
+++ VikingWalker/Assets/Scripts/CraftEngine.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftEngine : MonoBehaviour
+{
+    [SerializeField]string currentCraftItem;
+    [SerializeField] GameObject itemToSpawn;
+    [SerializeField, Range(10, 1000)] int itemPrice = 100;
+
+    public void CraftHome()
+    {
+        GameObject item = Resources.Load<GameObject>(currentCraftItem);
+        itemToSpawn = Instantiate(item);
+        Vector3 playerPos = GameObject.Find("Player").transform.position;
+        itemToSpawn.transform.position = playerPos;
+    }
+
+    public void SetCurrentCraftItem(string settingItem)
+    {
+        int goldValue = PlayerPrefs.GetInt("gold");
+
+        if (goldValue > itemPrice)
+        {
+            currentCraftItem = settingItem;
+            PlayerPrefs.SetString("currentCraftItem", currentCraftItem);
+            goldValue = goldValue - itemPrice;
+            PlayerPrefs.SetInt("gold", goldValue);
+            CraftHome();
+        }
+    }
+}
+
