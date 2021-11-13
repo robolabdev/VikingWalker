b6ce2fca72a62fa2c1395163aa2b2f100ec9a410
SaladCrab <darius.rimkus0414@gmail.com>
2021-11-13 12:05:15 +0200

    sd

--- VikingWalker/Assets/Scripts/Movement.cs
+++ VikingWalker/Assets/Scripts/Movement.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class Movement: MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start 
+    [SerializeField] CharacterController playerController;
+
+    void Start()
     {
-        
+        playerController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         float v = Input.GetAxis("Vertical") / 3;
         float h = Input.GetAxis("Horizontal") / 3;
-        transform.position = transform.position + new Vector3(h, 0, v);
+        Vector3 direction = transform.forward * v + transform.right * h;
+        playerController.SimpleMove(direction);
+        Debug.Log("Current speed = " + playerController.velocity.magnitude);
     }
 }
+
