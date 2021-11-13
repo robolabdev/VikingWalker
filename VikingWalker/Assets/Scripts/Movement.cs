1c79a72d76367722a3f6f411894a2cd6e6081c16
SaladCrab <darius.rimkus0414@gmail.com>
2021-11-13 12:27:38 +0200

    Update Movement.cs

--- VikingWalker/Assets/Scripts/Movement.cs
+++ VikingWalker/Assets/Scripts/Movement.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class Movement: MonoBehaviour
 {
     [SerializeField] CharacterController playerController;
-
+    public float playerHeightJump = 5f;
+    public Transform playerX;
+    public Transform playerZ;
+     
     void Start()
     {
         playerController = GetComponent<CharacterController>();
@@ -15,9 +18,20 @@ public class Movement: MonoBehaviour
     {
         float v = Input.GetAxis("Vertical") / 3;
         float h = Input.GetAxis("Horizontal") / 3;
+        float z = Input.GetAxis("Jump") / 3;
         Vector3 direction = transform.forward * v + transform.right * h;
+        Vector3 jump = transform.up * z + transform.up;
+        playerController.SimpleMove(jump);
         playerController.SimpleMove(direction);
-        Debug.Log("Current speed = " + playerController.velocity.magnitude);
+        Debug.Log("Current speed = " + playerController.velocity.magnitude);          
+    }
+
+    public void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += new Vector3(playerX, playerHeightJump, playerZ);
+        }
     }
 }
 
