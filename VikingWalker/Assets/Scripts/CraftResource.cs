d68057cb2d885568c23005f47eefde0e7df27b04
MartinBgame <martin.belyanov@outlook.com>
2021-12-04 11:15:19 +0200

    Added

---
+++ VikingWalker/Assets/Scripts/CraftResource.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftResource : MonoBehaviour
+{
+    enum Resource
+    {
+        wood,
+        steel,
+        gold,
+        diamonds
+    };
+    [SerializeField] Resource currentZoneResource;
+
+    public GameObject resourceObject;
+
+    [SerializeField, Range(1, 100)] float resourceValue = 100;
+
+    public void MinusValue()
+    {
+        resourceValue = resourceValue - 1;
+        if (resourceValue < 0)
+            Destroy(resourceObject);
+    }
+
+    public void AddPlayerScore()
+    {
+        int playerResource = PlayerPrefs.GetInt(currentZoneResource.ToString());
+        playerResource = playerResource + 1;
+        PlayerPrefs.SetInt(currentZoneResource.ToString(), playerResource);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.GetComponent<Movement>())
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                MinusValue();
+                AddPlayerScore();
+            }
+        }
+    }
+}
