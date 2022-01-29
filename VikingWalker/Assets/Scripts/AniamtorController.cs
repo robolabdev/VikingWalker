bd0acdb10c2adb5edc59d0572e0dafc0ba7087b6
Adiani <myrzaevaa19@gmail.com>
2022-01-29 17:01:18 +0200

    настроила аниматор

---
+++ VikingWalker/Assets/Scripts/AniamtorController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AniamtorController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentSpeed = GetComponentInParent<CharacterController>().velocity.magnitude;
+        GetComponent<Animator>().SetFloat("Speed", currentSpeed);
+        float random = Mathf.Sin(Time.time);
+        GetComponent<Animator>().SetFloat("Random", random);
+    }
+}
