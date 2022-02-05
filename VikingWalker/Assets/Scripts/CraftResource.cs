29af3689474ae357b0ecddeb2133ad344297f2b0
Adiani <myrzaevaa19@gmail.com>
2022-02-05 16:54:49 +0200

    добавили майнинг

---
+++ VikingWalker/Assets/Scripts/CraftResource.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftResource : MonoBehaviour
+{
+    // Start is called before the first frame update
+    enum Resource
+    {
+        wood,
+        steel,
+        gold,
+        diamonds,
+        stone 
+    };
+    [SerializeField] Resource currentZoneResource;
+    [SerializeField, Range(1, 100)] float resourceValue = 100;
+    public void MinusValue()
+    {
+        resourceValue = resourceValue - 1;
+        if (resourceValue < 0)
+            Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    public void AddPlayerScore()
+    {
+        int playerResource = PlayerPrefs.GetInt(currentZoneResource.ToString());
+        playerResource = playerResource+ 1;
+        PlayerPrefs.SetInt(currentZoneResource.ToString(), playerResource);
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.GetComponent<Movement>())
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                MinusValue();
+                AddPlayerScore();
+
+            }
+        }
+    }
+}
---
+++ VikingWalker/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text woodcraft;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        woodcraft.text = PlayerPrefs.GetInt("wood").ToString();
+    }
+}
