049db49e6d468722eead2108807514061f5ac98a
barrevdes <86661189+barrevdes@users.noreply.github.com>
2023-05-30 17:39:59 +0300

    e

--- VikingWalker/Assets/Scripts/Movement.cs
+++ VikingWalker/Assets/Scripts/Movement.cs
@@ -2,21 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move ment : MonoBehaviour
+public class Movement : MonoBehaviour
 {
 
     
-    // Start is called before the first frame update
-    void start  
-    {
-        
-    }
+    // Start is called before the first frame updat  }
 
     // Update is called once per frame
     void Update()
     {
-        float v = Input.GetAxis("Vertical");
-        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical")/5;
+        float h = Input.GetAxis("Horizontal")/5;
         transform.position = transform.position + new Vector3(v, 0, h);
+
     }
 }
